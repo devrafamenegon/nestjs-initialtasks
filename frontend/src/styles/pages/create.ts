318c5245449c1c6532904665c2845e4a5f57647d
Rafa menegon <dev.rafaelmenegon@gmail.com>
2021-05-25 23:54:26 -0300

    :sparkles: Page CreateTask

---
+++ frontend/src/styles/pages/create.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const CreateContainer = styled.div`
+  width: 100vw;
+  height: calc(100vh - 4rem);
+
+  padding: 2em 10vw;
+
+  background: var(--background);
+`;
+
+export { CreateContainer }
