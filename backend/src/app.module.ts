4c72f2f1fc7d22af70284eb22c11340f86bdecbb
Rafa menegon <dev.rafaelmenegon@gmail.com>
2021-05-31 22:38:33 -0300

    :hammer: CORS enabled

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,9 +7,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://rafael:YqG4laMTZ78x5DIh@cluster0.cytd6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    ),
+    MongooseModule.forRoot('mongodb://localhost:27017/tasksdb', {
+      useFindAndModify: false
+}),
     TasksModule,
   ],
   controllers: [AppController],
