ba3772e733ef8354b25c2fa0ea7fd028b434c213
Rafa menegon <dev.rafaelmenegon@gmail.com>
2021-06-02 00:19:58 -0300

    :hammer: Api fix url

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.API_URL || 'http://localhost:8080/'
+  baseURL: process.env.API_URL || 'http://localhost:8080'
 })
 
 export default api
