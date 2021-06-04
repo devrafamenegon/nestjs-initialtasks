2f5769283b5ca0b2162f3d74645e9cf3507f3b54
Rafa menegon <dev.rafaelmenegon@gmail.com>
2021-06-03 22:42:49 -0300

    :sparkles: Delete tasks

--- frontend-react/src/components/AllTasks/index.js
+++ frontend-react/src/components/AllTasks/index.js
@@ -1,11 +1,12 @@
 
 import { useState, useEffect } from 'react';
-import { getAll } from '../../services/endpoint';
+import { getAll, deleteTask } from '../../services/endpoint';
+import 'reactjs-popup/dist/index.css';
 
-import { HomeContainer, TasksContainer, TasksCards, TasksButtons, Button } from './styles';
+import { HomeContainer, TasksContainer, TasksCards, TasksButtons, Button, StyledPopup, PopupModal, PopupButtonClose, PopupModalHeader, PopupModalContent, PopupModalActions, PopupButton, StyledFaPlusSquare, ContainerFaPlusSquare } from './styles';
 
 export const AllTasks = ({setTaskId, setContent}) => {
-  const [tasks, setTasks] = useState([])
+  const [tasks, setTasks] = useState([]);
   
   useEffect(() => {
     const fetchTask = async() => {
@@ -21,6 +22,13 @@ export const AllTasks = ({setTaskId, setContent}) => {
     setContent('update');
   }
 
+  const handleDelete = async (tasks, close) => {
+    await deleteTask(tasks);
+    const tasksRes = await getAll();
+    setTasks(tasksRes.data);
+    close();
+  }
+
   return(
     <HomeContainer>
       <TasksContainer>
@@ -30,14 +38,33 @@ export const AllTasks = ({setTaskId, setContent}) => {
               <h3>{task.description}</h3>
               {task.completed ? "Tarefa completa" : "Tarefa pendente"}
               <TasksButtons>
-                <Button type="submit" isEdit={true} onClick={() => editClick(task._id)}>Editar</Button>
+                <Button isEdit={true} onClick={() => editClick(task._id)}>Editar</Button>
+                <StyledPopup trigger={<Button>Excluir</Button>} modal nested>
+                  {close => (
+                    <PopupModal>
+                      <PopupButtonClose onClick={close}>
+                        &times;
+                      </PopupButtonClose>
+                      <PopupModalHeader> 🧨 Deseja excluir esta tarefa? </PopupModalHeader>
+                      <PopupModalContent>
+                        <h3>{task.description}</h3>
+                        <span>{task.completed ? "Tarefa completa" : "Tarefa pendente"}</span>
+                      </PopupModalContent>
 
-                <Button isEdit={false}>Excluir</Button>
+                      <PopupModalActions>
+                        <PopupButton isYes={true} onClick={() => handleDelete(task._id, close)}>Sim</PopupButton>
+                        <PopupButton onClick={() => close()}>Não</PopupButton>
+                      </PopupModalActions>
+                    </PopupModal>
+                  )}
+                </StyledPopup>
               </TasksButtons>
             </li> 
           </TasksCards>
         ))}
-
+        <ContainerFaPlusSquare>
+          <StyledFaPlusSquare size="100%" onClick={() => setContent('create')}/>
+        </ContainerFaPlusSquare>
       </TasksContainer>
     </HomeContainer>
   );
--- frontend-react/src/components/AllTasks/styles.ts
+++ frontend-react/src/components/AllTasks/styles.ts
@@ -1,7 +1,14 @@
 import styled, {css} from "styled-components";
+import Popup from 'reactjs-popup';
+import { FaPlus } from "react-icons/fa";
+
 
 interface ButtonProps {
-  isEdit: boolean;
+  isEdit?: boolean;
+}
+
+interface ButtonPopupProps {
+  isYes?: boolean;
 }
 
 const HomeContainer = styled.div`
@@ -20,7 +27,7 @@ const TasksContainer = styled.div`
 `;
 
 const TasksCards = styled.ul`
-  width: 100%;
+  width: 225px;
   height: 10em;
 
   padding: 2em;
@@ -32,6 +39,24 @@ const TasksCards = styled.ul`
 
   background: var(--white);
   color: var(--navyblue);
+
+  animation: 0.5s showAnimation;
+  transition: all 1s;
+
+  h3 {
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+  }
+
+  @keyframes showAnimation {
+    from {
+      filter: opacity(0);
+    }
+    to {
+      filter: opacity(1);
+    }
+  }
 `;
 
 const TasksButtons = styled.div`
@@ -55,4 +80,92 @@ const Button = styled.button<ButtonProps>`
   `}
 `;
 
-export { HomeContainer, TasksContainer, TasksCards, TasksButtons, Button}
+const StyledPopup = styled(Popup)`
+  // use your custom style for ".popup-content"
+  &-content {
+    max-width: 400px;
+    min-width: 300px;
+    padding: 10px 20px;
+    border-radius: 10px;
+
+    animation: 0.5s showAnimationPopup;
+
+    @keyframes showAnimationPopup {
+    from {
+      filter: opacity(0);
+      transform: translateY(-50%);
+    }
+    to {
+      filter: opacity(1);
+      transform: translateY(0);
+    }
+  }
+  }
+`;
+
+const PopupModal = styled.div`
+  width: 100%;
+`;
+
+const PopupButtonClose = styled.button`
+  width: 25px;
+  height: 25px;
+
+  position: absolute;
+  top: 5px;
+  right: 5px;
+
+  background: var(--white);
+  border: 1px solid var(--navyblue);
+  border-radius: 5px;
+
+  transition: all 0.2s;
+
+  &:hover {
+    color: var(--white);
+    background: var(--redlava);
+    border: none;
+  }
+`;
+
+const PopupModalHeader = styled.div`
+  margin-top: 10px;
+`;
+
+const PopupModalContent = styled.div`
+margin-top: 20px;
+`;
+
+const PopupModalActions = styled.div`
+  margin-top: 10px;
+  button {
+    margin-right: 20px;
+  }
+`;
+
+const PopupButton = styled.button<ButtonPopupProps>`
+  height: 2em;
+  width: 100px;
+  border: none;
+  border-radius: 5px;
+
+  background: var(--redlava);
+  color: var(--simplewhite);
+
+  ${({ isYes }) => isYes && css`
+    background: var(--bluegrotto);
+  `}
+`;
+
+const ContainerFaPlusSquare = styled(TasksCards)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledFaPlusSquare = styled(FaPlus)`
+  color: var(--bluegrotto);
+  cursor: pointer;
+`;
+
+export { HomeContainer, TasksContainer, TasksCards, TasksButtons, Button, PopupModal, PopupButtonClose, PopupModalHeader, PopupModalContent, PopupModalActions, StyledPopup, PopupButton, StyledFaPlusSquare, ContainerFaPlusSquare }
